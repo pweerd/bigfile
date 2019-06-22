b66f707479887201513aef480931d9778a30162c
Peter van der Weerd <pweerd@bitmanager.nl>
2019-06-22 22:44:48 +0200

    Using TextRenderer instead of a bitmap to get the pixels size of a string. This also gives better result.

--- Source/FixedFontMeasures.cs
+++ Source/FixedFontMeasures.cs
@@ -17,57 +17,59 @@
  * under the License.
  */
 
+using Bitmanager.Core;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Bitmanager.BigFile
 {
-    public class FixedFontMeasures
-    {
-        private double perChar;
-        public FixedFontMeasures(Font f)
-        {
-            using (var image = new Bitmap(1, 1))
-            {
-                using (var g = Graphics.FromImage(image))
-                {
-                    var sb = new StringBuilder();
-                    for (int j = 0; j < 26; j++)
-                        sb.Append((char)'a' + j).Append((char)'A' + j);
-                    for (int j = 0; j < 10; j++)
-                        sb.Append((char)'0' + j);
-                    sb.Append(" .,'");
-                    perChar = g.MeasureString(sb.ToString(), f).Width / sb.Length;
-                }
-            }
-        }
+   /// <summary>
+   /// Holds char information for a fixed font
+   /// Used to determin the #chars for a number of pixels, or the #pixels for a number of chars
+   /// </summary>
+   public class FixedFontMeasures
+   {
+      private readonly double perChar;
+      public FixedFontMeasures(Font f)
+      {
+         var sb = new StringBuilder();
+         for (int j = 0; j < 26; j++)
+            sb.Append((char)'a' + j).Append((char)'A' + j);
+         for (int j = 0; j < 10; j++)
+            sb.Append((char)'0' + j);
+         sb.Append(" .,'");
+         String x = sb.ToString();
+         perChar = TextRenderer.MeasureText(x, f).Width / x.Length;
+      }
 
       public int GetTextPixels(String x, int add = 0)
       {
          return GetTextPixels(x == null ? 0 : x.Length, add);
       }
+
       public int GetTextPixels(int strlen, int add = 0)
       {
          const int MAX = 0x7FFF;  //short
-         double ret = strlen;
-         ret *= perChar * 1.01;
-         ret += add;
+         double ret = strlen * perChar + add; // * 1.01
+
          return ret > MAX ? MAX : (int)ret;
       }
-      public int GetTextPixels(String x, String y, int add=0)
-        {
-            return Math.Max(GetTextPixels(x, add), GetTextPixels(y, add));
-        }
 
-        public int GetTextLengthForPixels(int pixels)
-        {
-            if (pixels > 0xFFFF) pixels = 0xFFFF;
-            else if (pixels < 0) return 0;
-            return (int)(pixels / perChar); 
-        }
-    }
+      public int GetTextPixels(String x, String y, int add = 0)
+      {
+         return Math.Max(GetTextPixels(x, add), GetTextPixels(y, add));
+      }
+
+      public int GetTextLengthForPixels(int pixels)
+      {
+         if (pixels > 0xFFFF) pixels = 0xFFFF;
+         else if (pixels < 0) return 0;
+         return (int)(pixels / perChar);
+      }
+   }
 }
