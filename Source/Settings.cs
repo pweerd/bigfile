f125dc9d769f43a5871bd2d9aae2e6b2eedde81a
Peter van der Weerd <pweerd@bitmanager.nl>
2022-05-08 11:17:42 +0200

    Write all history entries to the registry (also the null's) Reformatted

--- Source/Settings.cs
+++ Source/Settings.cs
@@ -24,14 +24,12 @@ using Bitmanager.Core;
 using Bitmanager.IO;
 using Microsoft.Win32;
 
-namespace Bitmanager.BigFile
-{
+namespace Bitmanager.BigFile {
    /// <summary>
    /// Readonly object with all current settings replaced with their defaults
    /// Created from a SettingsSource
    /// </summary>
-   public class Settings
-   {
+   public class Settings {
       public readonly SettingsSource Source;
       public readonly long TotalPhysicalMemory;
       public readonly long AvailablePhysicalMemory;
@@ -48,14 +46,13 @@ namespace Bitmanager.BigFile
       public readonly int MaxCopyLines;
       public readonly int MaxCopySize;
 
-      public Settings(SettingsSource src)
-      {
+      public Settings (SettingsSource src) {
          Source = src;
          TotalPhysicalMemory = src.TotalPhysicalMemory;
          AvailablePhysicalMemory = src.AvailablePhysicalMemory;
          Color tmp = src.HighlightColor;
          HighlightColor = tmp;
-         SelectedHighlightColor = Color.FromArgb((int)(tmp.R * .6f), (int)(tmp.G * .6f), (int)(tmp.B * .6f));
+         SelectedHighlightColor = Color.FromArgb ((int)(tmp.R * .6f), (int)(tmp.G * .6f), (int)(tmp.B * .6f));
 
          ContextColor = src.ContextColor;
          NumContextLines = src.NumContextLines;
@@ -70,15 +67,15 @@ namespace Bitmanager.BigFile
 
       public void Dump () {
          var logger = Globals.SettingsLogger;
-         logger.Log("Dumping current ACTUAL settings:");
-         logger.Log("-- HighlightColor: {0}, SelectedHighlightColor: {1}", HighlightColor, SelectedHighlightColor);
-         logger.Log("-- ContextColor: {0}, NumContextLines={1}", ContextColor, NumContextLines);
-         logger.Log("-- MaxCopyLines: {0}, MaxCopySize={1} ({2})", MaxCopyLines, MaxCopySize, Pretty.PrintSize(MaxCopySize));
-         logger.Log("-- MaxLineLength: {0} ({1})", MaxLineLength, Pretty.PrintSize(MaxLineLength));
-         logger.Log("-- SearchThreads: {0}", SearchThreads);
-         logger.Log("-- CompressMemoryIfBigger: {0} ({1})", CompressMemoryIfBigger, Pretty.PrintSize(CompressMemoryIfBigger));
-         logger.Log("-- LoadMemoryIfBigger: {0} ({1})", LoadMemoryIfBigger, Pretty.PrintSize(LoadMemoryIfBigger));
-         logger.Log("-- PhysicalMem: total={0}, available={1}", Pretty.PrintSize(TotalPhysicalMemory), Pretty.PrintSize(AvailablePhysicalMemory));
+         logger.Log ("Dumping current ACTUAL settings:");
+         logger.Log ("-- HighlightColor: {0}, SelectedHighlightColor: {1}", HighlightColor, SelectedHighlightColor);
+         logger.Log ("-- ContextColor: {0}, NumContextLines={1}", ContextColor, NumContextLines);
+         logger.Log ("-- MaxCopyLines: {0}, MaxCopySize={1} ({2})", MaxCopyLines, MaxCopySize, Pretty.PrintSize (MaxCopySize));
+         logger.Log ("-- MaxLineLength: {0} ({1})", MaxLineLength, Pretty.PrintSize (MaxLineLength));
+         logger.Log ("-- SearchThreads: {0}", SearchThreads);
+         logger.Log ("-- CompressMemoryIfBigger: {0} ({1})", CompressMemoryIfBigger, Pretty.PrintSize (CompressMemoryIfBigger));
+         logger.Log ("-- LoadMemoryIfBigger: {0} ({1})", LoadMemoryIfBigger, Pretty.PrintSize (LoadMemoryIfBigger));
+         logger.Log ("-- PhysicalMem: total={0}, available={1}", Pretty.PrintSize (TotalPhysicalMemory), Pretty.PrintSize (AvailablePhysicalMemory));
       }
    }
 
@@ -94,144 +91,130 @@ namespace Bitmanager.BigFile
       public readonly long TotalPhysicalMemory;
       public readonly long AvailablePhysicalMemory;
 
-      public readonly SizeSetting MaxLineLength = new SizeSetting("max_line_size", "10MB");
-      public readonly ColorSetting HighlightColor = new ColorSetting("highlight_color", Color.Lime);
-      public readonly ColorSetting ContextColor = new ColorSetting("context_color", Color.LightGray);
-      public readonly IntSetting NumContextLines = new IntSetting("context_lines", "0");
+      public readonly SizeSetting MaxLineLength = new SizeSetting ("max_line_size", "10MB");
+      public readonly ColorSetting HighlightColor = new ColorSetting ("highlight_color", Color.Lime);
+      public readonly ColorSetting ContextColor = new ColorSetting ("context_color", Color.LightGray);
+      public readonly IntSetting NumContextLines = new IntSetting ("context_lines", "0");
 
-      public readonly ThreadsSetting SearchThreads = new ThreadsSetting("search_threads", "0", AUTO);
-      public readonly SizeSetting LoadMemoryIfBigger = new SizeSetting("load_memory", "32GB", AUTO);
-      public readonly SizeSetting CompressMemoryIfBigger = new SizeSetting("compress_memory", "1GB", AUTO);
+      public readonly ThreadsSetting SearchThreads = new ThreadsSetting ("search_threads", "0", AUTO);
+      public readonly SizeSetting LoadMemoryIfBigger = new SizeSetting ("load_memory", "32GB", AUTO);
+      public readonly SizeSetting CompressMemoryIfBigger = new SizeSetting ("compress_memory", "1GB", AUTO);
 
-      public readonly IntSetting MaxCopyLines = new IntSetting("max_copy_lines", "100000");
-      public readonly SizeSetting MaxCopySize = new SizeSetting("max_copy_size", "100MB");
+      public readonly IntSetting MaxCopyLines = new IntSetting ("max_copy_lines", "100000");
+      public readonly SizeSetting MaxCopySize = new SizeSetting ("max_copy_size", "100MB");
 
-      public SettingsSource(bool autoLoad=false)
-      {
+      public SettingsSource (bool autoLoad = false) {
          if (autoLoad)
-            Load();
+            Load ();
 
-         var ci = new Microsoft.VisualBasic.Devices.ComputerInfo();
+         var ci = new Microsoft.VisualBasic.Devices.ComputerInfo ();
          this.TotalPhysicalMemory = (long)ci.TotalPhysicalMemory;
          this.AvailablePhysicalMemory = (long)Math.Min (ci.AvailablePhysicalMemory, ci.AvailableVirtualMemory);
-         Globals.MainLogger.Log("Total memory={0}, available={1}.", Pretty.PrintSize(TotalPhysicalMemory), Pretty.PrintSize(AvailablePhysicalMemory));
+         Globals.MainLogger.Log ("Total memory={0}, available={1}.", Pretty.PrintSize (TotalPhysicalMemory), Pretty.PrintSize (AvailablePhysicalMemory));
 
-         ActualizeDefaults();
+         ActualizeDefaults ();
       }
 
 
 
-      public Settings ActualizeDefaults()
-      {
-         return Settings = new Settings(this); //actualize settings
+      public Settings ActualizeDefaults () {
+         return Settings = new Settings (this); //actualize settings
       }
 
-      public void Load()
-      {
+      public void Load () {
          var rootKey = Registry.CurrentUser;
-         using (var key = rootKey.OpenSubKey(_KEY, false))
-         {
-            MaxLineLength.Load(key);
-            ContextColor.Load(key);
-            HighlightColor.Load(key);
-            NumContextLines.Load(key);
-            MaxCopyLines.Load(key);
-            MaxCopySize.Load(key);
-            SearchThreads.Load(key);
-            LoadMemoryIfBigger.Load(key);
-            CompressMemoryIfBigger.Load(key);
+         using (var key = rootKey.OpenSubKey (_KEY, false)) {
+            MaxLineLength.Load (key);
+            ContextColor.Load (key);
+            HighlightColor.Load (key);
+            NumContextLines.Load (key);
+            MaxCopyLines.Load (key);
+            MaxCopySize.Load (key);
+            SearchThreads.Load (key);
+            LoadMemoryIfBigger.Load (key);
+            CompressMemoryIfBigger.Load (key);
 
-            FormLine.LoadState(key);
+            FormLine.LoadState (key);
          }
       }
 
-      public void Save()
-      {
+      public void Save () {
          var rootKey = Registry.CurrentUser;
-         using (var key = rootKey.CreateSubKey(_KEY, true))
-         {
-            MaxLineLength.Save(key);
-            ContextColor.Save(key);
-            HighlightColor.Save(key);
-            NumContextLines.Save(key);
-            MaxCopyLines.Save(key);
-            MaxCopySize.Save(key);
-            SearchThreads.Save(key);
-            LoadMemoryIfBigger.Save(key);
-            CompressMemoryIfBigger.Save(key);
+         using (var key = rootKey.CreateSubKey (_KEY, true)) {
+            MaxLineLength.Save (key);
+            ContextColor.Save (key);
+            HighlightColor.Save (key);
+            NumContextLines.Save (key);
+            MaxCopyLines.Save (key);
+            MaxCopySize.Save (key);
+            SearchThreads.Save (key);
+            LoadMemoryIfBigger.Save (key);
+            CompressMemoryIfBigger.Save (key);
 
-            FormLine.SaveState(key);
+            FormLine.SaveState (key);
          }
-         Load();
+         Load ();
       }
 
-      public void Dump(String why) {
+      public void Dump (String why) {
          var logger = Globals.SettingsLogger;
-         logger.Log("Dumping current registry settings ({0}):", why);
-         logger.Log("-- {0}", HighlightColor);
-         logger.Log("-- {0}, {1}", ContextColor, NumContextLines);
-         logger.Log("-- {0}, {1}", MaxCopyLines, MaxCopySize);
-         logger.Log("-- {0}", MaxLineLength);
-         logger.Log("-- {0}", SearchThreads);
-         logger.Log("-- {0}", CompressMemoryIfBigger);
-         logger.Log("-- {0}", LoadMemoryIfBigger);
-         logger.Log("-- PhysicalMem: total={0}, available={1}", Pretty.PrintSize(TotalPhysicalMemory), Pretty.PrintSize(AvailablePhysicalMemory));
-         if (Settings != null) Settings.Dump();
+         logger.Log ("Dumping current registry settings ({0}):", why);
+         logger.Log ("-- {0}", HighlightColor);
+         logger.Log ("-- {0}, {1}", ContextColor, NumContextLines);
+         logger.Log ("-- {0}, {1}", MaxCopyLines, MaxCopySize);
+         logger.Log ("-- {0}", MaxLineLength);
+         logger.Log ("-- {0}", SearchThreads);
+         logger.Log ("-- {0}", CompressMemoryIfBigger);
+         logger.Log ("-- {0}", LoadMemoryIfBigger);
+         logger.Log ("-- PhysicalMem: total={0}, available={1}", Pretty.PrintSize (TotalPhysicalMemory), Pretty.PrintSize (AvailablePhysicalMemory));
+         if (Settings != null) Settings.Dump ();
       }
 
 
-      public static long GetActualSize (String size, String auto)
-      {
-         size = size.TrimToNull();
+      public static long GetActualSize (String size, String auto) {
+         size = size.TrimToNull ();
          if (size == null) size = auto;
-         switch (size.ToLowerInvariant())
-         {
+         switch (size.ToLowerInvariant ()) {
             case "auto":
-               return GetActualSize(auto, "off");
+               return GetActualSize (auto, "off");
             case "off":
                return long.MaxValue;
             case "on":
                return -1;
             default:
-               return Pretty.ParseSize(size);
+               return Pretty.ParseSize (size);
          }
       }
 
-      public static void SaveFormPosition(int left, int top, int w, int h)
-      {
-         String sizeStr = Invariant.Format("{0};{1};{2};{3}", w, h, left, top);
+      public static void SaveFormPosition (int left, int top, int w, int h) {
+         String sizeStr = Invariant.Format ("{0};{1};{2};{3}", w, h, left, top);
          var rootKey = Registry.CurrentUser;
-         using (var key = rootKey.CreateSubKey(_KEY, true))
-         {
-            WriteVal(key, "position", sizeStr);
+         using (var key = rootKey.CreateSubKey (_KEY, true)) {
+            WriteVal (key, "position", sizeStr);
          }
       }
 
-      public static bool LoadFormPosition(out int left, out int top, out int w, out int h)
-      {
+      public static bool LoadFormPosition (out int left, out int top, out int w, out int h) {
          String sizeStr;
          var rootKey = Registry.CurrentUser;
-         using (var key = rootKey.CreateSubKey(_KEY, false))
-         {
-            sizeStr = ReadVal(key, "position", null);
+         using (var key = rootKey.CreateSubKey (_KEY, false)) {
+            sizeStr = ReadVal (key, "position", null);
          }
-         Globals.MainLogger.Log("Loaded size str={0}", sizeStr);
+         Globals.MainLogger.Log ("Loaded size str={0}", sizeStr);
 
          left = -1;
          top = -1;
          w = -1;
          h = -1;
-         if (String.IsNullOrEmpty(sizeStr)) return false;
+         if (String.IsNullOrEmpty (sizeStr)) return false;
 
-         String[] parts = sizeStr.Split(';');
-         for (int i=0; i<parts.Length; i++)
-         {
+         String[] parts = sizeStr.Split (';');
+         for (int i = 0; i < parts.Length; i++) {
             int v;
-            if (!int.TryParse(parts[i], out v)) break;
+            if (!int.TryParse (parts[i], out v)) break;
             if (v <= 0) break;
 
-            switch (i)
-            {
+            switch (i) {
                case 0: w = v; continue;
                case 1: h = v; continue;
                case 2: left = v; continue;
@@ -242,78 +225,63 @@ namespace Bitmanager.BigFile
          return (w >= 0);
       }
 
-      public static void SaveFileHistory (String[] list, String prefix)
-      {
+      public static void SaveFileHistory (String[] list, String prefix) {
          var rootKey = Registry.CurrentUser;
-         using (var key = rootKey.CreateSubKey(_KEY, true))
-         {
-            for (int i=0; i<list.Length; i++)
-            {
-               if (list[i] == null) break;
-               WriteVal(key, prefix + i.ToString(), list[i]);
+         using (var key = rootKey.CreateSubKey (_KEY, true)) {
+            for (int i = 0; i < list.Length; i++) {
+               WriteVal (key, prefix + i.ToString (), list[i]);
             }
          }
       }
-      public static String[] LoadFileHistory(String prefix)
-      {
+      public static String[] LoadFileHistory (String prefix) {
          String[] ret = new string[10];
          var rootKey = Registry.CurrentUser;
-         using (var key = rootKey.CreateSubKey(_KEY, false))
-         {
-            for (int i = 0; i < ret.Length; i++)
-            {
-               ret[i] = ReadVal(key, prefix + i.ToString(), null);
+         using (var key = rootKey.CreateSubKey (_KEY, false)) {
+            for (int i = 0; i < ret.Length; i++) {
+               ret[i] = ReadVal (key, prefix + i.ToString (), null);
                if (ret[i] == null) break;
             }
          }
          return ret;
       }
 
-      public static String ReadVal(RegistryKey key, String valName, String def)
-      {
+      public static String ReadVal (RegistryKey key, String valName, String def) {
          if (key == null) return def;
-         var v = key.GetValue(valName, def);
+         var v = key.GetValue (valName, def);
          if (v == null) return def;
 
-         String ret = v.ToString();
-         return String.IsNullOrEmpty(ret) ? def : ret;
+         String ret = v.ToString ();
+         return String.IsNullOrEmpty (ret) ? def : ret;
       }
-      public static Color ReadVal(RegistryKey key, String valName, Color def)
-      {
-         return ColorTranslator.FromHtml(ReadVal(key, valName, ColorTranslator.ToHtml(def)));
+      public static Color ReadVal (RegistryKey key, String valName, Color def) {
+         return ColorTranslator.FromHtml (ReadVal (key, valName, ColorTranslator.ToHtml (def)));
       }
-      public static int ReadVal(RegistryKey key, String valName, int def)
-      {
+      public static int ReadVal (RegistryKey key, String valName, int def) {
          if (key == null) return def;
-         var v = key.GetValue(valName, def);
+         var v = key.GetValue (valName, def);
          return v == null ? def : (int)v;
       }
-      public static bool ReadVal(RegistryKey key, String valName, bool def)
-      {
+      public static bool ReadVal (RegistryKey key, String valName, bool def) {
          if (key == null) return def;
-         var v = key.GetValue(valName, def ? 1 : 0);
+         var v = key.GetValue (valName, def ? 1 : 0);
          return v == null ? def : (0 != (int)v);
       }
-      public static void WriteVal(RegistryKey key, String valName, String val)
-      {
+      public static void WriteVal (RegistryKey key, String valName, String val) {
          if (key == null) return;
          if (val == null) val = String.Empty;
-         key.SetValue(valName, val);
+         key.SetValue (valName, val);
       }
-      public static void WriteVal(RegistryKey key, String valName, int val)
-      {
+      public static void WriteVal (RegistryKey key, String valName, int val) {
          if (key == null) return;
-         key.SetValue(valName, val);
+         key.SetValue (valName, val);
       }
-      public static void WriteVal(RegistryKey key, String valName, bool val)
-      {
+      public static void WriteVal (RegistryKey key, String valName, bool val) {
          if (key == null) return;
-         key.SetValue(valName, val ? 1 : 0);
+         key.SetValue (valName, val ? 1 : 0);
       }
-      public static void WriteVal(RegistryKey key, String valName, Color val)
-      {
+      public static void WriteVal (RegistryKey key, String valName, Color val) {
          if (key == null) return;
-         WriteVal(key, valName, ColorTranslator.ToHtml(val));
+         WriteVal (key, valName, ColorTranslator.ToHtml (val));
       }
 
    }
